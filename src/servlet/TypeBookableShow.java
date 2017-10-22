e9127838475d44af16271ec068e51bde5ba424a5
Ezequiel Leonardo Castaño <elcg@gmx.com>
2017-10-22 18:15:00 -0300

    Add Servlet structure

---
+++ src/servlet/TypeBookableShow.java
@@ -0,0 +1,23 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet({ "/TypeBookable/Show" })
+public class TypeBookableShow extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public TypeBookableShow() {}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("WEB-INF/login.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("WEB-INF/login.jsp").forward(request, response);
+	}
+}
