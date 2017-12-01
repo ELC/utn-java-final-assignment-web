63fdc505f54db1bd28f62c1700d31e007a05a73d
Ezequiel Leonardo Castaño <elcg@gmx.com>
2017-12-01 12:16:38 -0300

    Add login filter with error page

---
+++ WebContent/403.jsp
@@ -0,0 +1,17 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="t" tagdir="/WEB-INF/tags" %>
+
+<t:base>
+    <jsp:body>
+	<section class="dialog box" style="text-align: center;">
+	    <div class="container clearfix">
+	        <h1>
+	            <i class="large fa fa-bug"></i>403
+	        </h1>
+	        <h2>Access Denied</h2>
+	        <h3>We're Sorry but only special users can access this page. Please, <a href="#" data-toggle="modal" data-target="#login-modal">log in</a> or contact an administrator</h3>
+	    </div>
+	</section>
+
+    </jsp:body>
+</t:base>
---
+++ src/servlet/LogInFilter.java
@@ -0,0 +1,38 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter("/Booking/*")
+public class LogInFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig config) throws ServletException {}
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpSession session = request.getSession(false);
+
+        if (session == null || session.getAttribute("user") == null) {
+        	request.getRequestDispatcher("/403.jsp").forward(request, response);
+        } else {
+            chain.doFilter(req, res); 
+        }
+    }
+
+    @Override
+    public void destroy() {}
+
+}
